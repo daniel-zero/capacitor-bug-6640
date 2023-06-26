02af1675c30cfae59f77bfa6261af45d30494bc5
Daniel Sogl <daniel.sogl@finanzen-zero.net>
2023-06-26 22:33:20 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'capacitor-bug-demo',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
